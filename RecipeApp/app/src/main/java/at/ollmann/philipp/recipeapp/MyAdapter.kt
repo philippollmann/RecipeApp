9aac67845c1d4a18f716dd4b132b2a220273aa6a
Philipp Ollmann <ollmann.ph@gmail.com>
2021-10-27 13:05:09 +0200

    03-changed row item layout

--- RecipeApp/app/src/main/java/at/ollmann/philipp/recipeapp/MyAdapter.kt
+++ RecipeApp/app/src/main/java/at/ollmann/philipp/recipeapp/MyAdapter.kt
@@ -4,18 +4,22 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class MyAdapter(val context: Context, val userList: List<MyDataItem>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         var userId: TextView
         var title: TextView
+        var imageView: ImageView
 
         init {
             userId = itemView.findViewById(R.id.userId)
             title = itemView.findViewById(R.id.title)
+            imageView = itemView.findViewById(R.id.imageView)
         }
     }
 
@@ -27,6 +31,11 @@ class MyAdapter(val context: Context, val userList: List<MyDataItem>) : Recycler
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.userId.text = userList[position].userId.toString()
         holder.title.text = userList[position].title.toString()
+        Picasso.with(this.context)
+            .load("https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80")
+            .placeholder(R.mipmap.ic_launcher)
+            .resize(300,300)
+            .into(holder.imageView)
     }
 
     override fun getItemCount(): Int {
