54775255337c0ca7d436c36bd12ce5cd525492fc
Philipp Ollmann <ollmann.ph@gmail.com>
2021-10-27 22:12:59 +0200

    08 - added RecipeDetails activity

---
+++ RecipeApp/app/src/main/java/at/ollmann/philipp/recipeapp/RecipeDetails.kt
@@ -0,0 +1,11 @@
+package at.ollmann.philipp.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_details)
+    }
+}
